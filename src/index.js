314a9ba1c88721bff929a545b855a646952f32ae
wangbaijuan <wangbaijuan@qq.com>
2017-11-14 11:38:05 +0800

    hot loader www.npmjs.com/package/react-hot-loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { AppContainer } from 'react-hot-loader'
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const render = (Component) => {
+    ReactDOM.render(
+        <AppContainer>
+            <Component/>
+        </AppContainer>
+        , document.getElementById('root')
+    )
+};
+
+render(App);
+
+// Webpack Hot Module Replacement API
+if (module.hot) {
+    module.hot.accept('./App', () => { render(App) })
+}
